47c7750e62f7eeae905f96fbaa8f2156f442c4cd
gicck <gonzalo.calle.clavel@gmail.com>
2020-08-15 23:42:02 -0400

    Add initial API project

---
+++ food-bank-API/src/main/java/com/foodbank/foodbankapi/FoodBankApiApplication.java
@@ -0,0 +1,13 @@
+package com.foodbank.foodbankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodBankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodBankApiApplication.class, args);
+	}
+
+}
