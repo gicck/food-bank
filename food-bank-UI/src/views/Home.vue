d159ad2735dd38d4d7dd71ce0ab3752d6aff2438
gicck <gonzalo.calle.clavel@gmail.com>
2020-08-22 12:05:34 -0400

    Initial windmill html template

--- food-bank-UI/src/views/Home.vue
+++ food-bank-UI/src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Welcome to Your Vue.js App" class="flex flex-col flex-1 w-full" />
   </div>
 </template>
 
