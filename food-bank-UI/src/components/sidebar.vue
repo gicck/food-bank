dcd1927b0b9c4596ab0acdfcdfb0b659f5ff1674
gicck <gonzalo.calle.clavel@gmail.com>
2020-11-01 00:25:12 -0400

    Add donors and recipients

--- food-bank-UI/src/components/sidebar.vue
+++ food-bank-UI/src/components/sidebar.vue
@@ -86,7 +86,7 @@
             </router-link>
           </li>
           <li class="relative px-6 py-3">
-            <router-link to="/deliveryView" class="nav-link">
+            <router-link to="/recipientView" class="nav-link">
               <a
                 class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800"
               >
@@ -104,6 +104,28 @@
                   ></path>
                 </svg>
                 <span class="ml-4">Beneficiarios</span>
+              </a>
+            </router-link>
+          </li>
+          <li class="relative px-6 py-3">
+            <router-link to="/donorView" class="nav-link">
+              <a
+                class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800"
+              >
+                <svg
+                  class="w-5 h-5"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 25 25"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
+                  ></path>
+                </svg>
+                <span class="ml-4">Donantes</span>
               </a>
             </router-link>
           </li>
--- food-bank-UI/src/router/index.js
+++ food-bank-UI/src/router/index.js
@@ -62,7 +62,25 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/DeliveryCreate.vue")
-  }
+  },
+  {
+    path: "/recipientView",
+    name: "Recipients",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Recipients.vue")
+  },
+  {
+    path: "/donorView",
+    name: "Donors",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Donors.vue")
+  }  
 ];
 
 const router = new VueRouter({
